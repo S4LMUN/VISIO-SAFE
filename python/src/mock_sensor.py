e86348f27dae2e73afbcbed04958642f31d1689b
S4LMUN <chayutpong2154@gmail.com>
2026-09-01 21:25:05 +0700

    fix: sensor logic

--- python/src/mock_sensor.py
+++ python/src/mock_sensor.py
@@ -5,15 +5,15 @@
 # danger level 1 simple, 2 near, 3 danger 
 
 def sensor():
-    sensor_r = 100
-    if sensor_r >= 2 < 5:
+    sensor_r = 100 # Follow sensor device #
+    if sensor_r < 5 and sensor_r >= 2:
         return 1
 
-    elif sensor_r >= 1 < 2:
+    elif sensor_r < 2 and sensor_r >= 1 :
         return 2
 
     elif sensor_r < 1:
         return 3
 
     else:
-        return None
+        return 0
